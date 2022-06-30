c813ce5eab28af14153f41b93c6caae89ba147c0
ibrahim karahan <karahan930@gmail.com>
2022-06-30 10:54:12 +0300

    [feat] Implement example-service load test scenario

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # load-test
 Example load-test with k6
+
+# Installation
+
+### `brew install k6`
+
+For the other operating system, you can check [here](https://k6.io/docs/getting-started/installation/).
+
+# Run
+
+### `k6 run ./example-service/load-test`
---
+++ example-serivce/load-test.js
@@ -0,0 +1,24 @@
+import http from 'k6/http';           // import dependencies
+import {sleep, check, group} from 'k6';
+
+export const options = {       // define test stages and duration
+    stages: [
+        {target: 5, duration: '10s'},  // ramp-up
+        {target: 10, duration: '20s'},  // peak point
+        {target: 0, duration: '10s'},  // ramp-down
+    ],
+};
+
+export default function () {     // VU code
+    group('example-service load testing', function () {
+        group("example-endpoint load test", function (){
+            let res = http.get(SERVICE_URL);
+
+            check(res, {
+                "status is 200": (r) => r.status === 200
+            });
+        })
+    });
+
+    sleep(1);
+}
